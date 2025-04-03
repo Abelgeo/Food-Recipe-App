62cb38ca72368570e8a4310e9e86e273c7cf9a66
Abel George Wilson <abelsalem5102004@gmail.com>
2025-04-03 10:37:31 +0530

    Merge branch 'master' of https://github.com/Abelgeo/Food-Recipe-App

--- App.js
+++ App.js
@@ -1,11 +1,7 @@
-import { StatusBar } from 'expo-status-bar';
 import AppNavigation from './src/navigation';
 
 export default function App() {
   return (
-    <>
-      <StatusBar style="dark" />
-      <AppNavigation />
-    </>
+    <AppNavigation />
   );
 }
